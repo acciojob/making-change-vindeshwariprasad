f84c46dab7cb701e41eab24c6333b86532d7c232
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-26 04:05:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const makeChange = (c) => {
+	let a = {"q":0,"d":0,"n":0,"p":0};
+	
+	let b = Math.floor(c/25);
+	c-=(25*b);
+	let kk = Math.floor(c/10);
+	c-=(10*kk)
+	let e = Math.floor(c/5);
+	c-=(5*e);
+	let f = Math.floor(c/1);
+	c-=(1*f);
+	a["q"] = b;
+	a["d"] = kk;
+	a["n"] = e;
+	a["p"] = f;
+	return a;
+	
+		
+
   // your name here
 };
 
-// Do not the change the code below
+// // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
